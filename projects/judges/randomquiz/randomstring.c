f2436d3eaf4912f50c83d617a0397d4288bda797
Shae Judge <judges@flip3.engr.oregonstate.edu>
2017-05-01 20:11:35 -0700

    Added randomquiz

---
+++ projects/judges/randomquiz/randomstring.c
@@ -0,0 +1,13 @@
+/*
+Shae Judge - CS362
+Random Test Quiz 2
+
+I created the random character and the random string generator functions.
+I realized that there were a certain amount of ASCII characters that would have to correspond
+to the proper characters that I would be randomly generating. When the proper combination of
+random character and random string was achieved, I was able to see the error message in 
+my console window.
+
+
+
+*/
